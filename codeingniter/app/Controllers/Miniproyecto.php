86316278172580e9c20119aeb17e3fca16d324c8
luzmaria0500 <84035133+luzmaria0500@users.noreply.github.com>
2021-06-17 11:29:35 -0500

    Update Miniproyecto.php

--- codeingniter/app/Controllers/Miniproyecto.php
+++ codeingniter/app/Controllers/Miniproyecto.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Controllers;
-use App\Models\GeneralModel;
+use App\Models\DatosModel;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-$gModel = new GeneralModel( );
+$gModel = new DatosModel( );
 $mensaje = session('mensaje')
 $datos = $gModel->listarTodo( );
 $data = ["datos" => $datos,
@@ -17,7 +17,7 @@ return view ('listado' ,$data);
 
 Public function obtenerDatos($id){
 
-$gModel = new GeneralModel( );
+$gModel = new DatosModel( );
 $datos = ["id" => $id];
 $respuesta = $gModel->obtenerInformacion($data);
 
@@ -27,26 +27,30 @@ return view('actualizar, $datos);
 }
 
 Public function insertar(){
-$gModel = new GeneralModel( );
-$data = [
-"nombre" => $_POST['nombre'], " a_paterno" => $_POST['apaterno'],
+$gModel = new DatosModel( );
+$data = ["nombre" => $_POST['nombre'],
+" a_paterno" => $_POST['apaterno'],
 "a_materno" => $_POST['amaterno'],
 
 ];
 
 $respuesta = $gModel -> insertar($data);
+
 if ( $respuesta > 0){
 return redirect( )->to(base_url('/index.php'))->With('mensajes','0');
 }else{
 
 return redirect( )->to(base_url('/index.php'))->With('mensajes','1');
 }
+
+
 }
 
 Public function actualizar(){
-$gModel = new GeneralModel( );
+$gModel = new DatosModel( );
 $data = [
-"nombre" => $_POST['nombre'], " a_paterno" => $_POST['apaterno'],
+"nombre" => $_POST['nombre'],
+" a_paterno" => $_POST['apaterno'],
 "a_materno" => $_POST['amaterno'],
 
 ];
@@ -60,10 +64,12 @@ return redirect( )->to(base_url('/index.php'))->With('mensajes','2');
 
 return redirect( )->to(base_url('/index.php'))->With('mensajes','3');
 }
+
+
 }
 
 Public function eliminar($dPersona){
-$gModel = new GeneralModel( );
+$gModel = new DatosModel( );
 $id =["id" => $idPersona];
 $respuesta = $gModel->eliminar ($id);
 
@@ -72,6 +78,12 @@ return redirect( )->to(base_url('/index.php'))->With('mensajes','4');
 }else{
 
 return redirect( )->to(base_url('/index.php'))->With('mensajes','5');
+
 }
+
+
 }
+
+
+
 }
